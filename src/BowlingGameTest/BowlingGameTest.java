28d34f2cb6d2d4b5fa2041fa70811dbabbb621bb
JeonMinGeon <wjsxorjs@ajou.ac.kr>
2021-04-01 17:33:44 +0900

    JAVA File Test

---
+++ src/BowlingGameTest/BowlingGameTest.java
@@ -0,0 +1,12 @@
+package BowlingGameTest;
+
+public class BowlingGameTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+		System.out.printf("Hello, World!");
+		System.out.println("Hello, World!");
+	}
+
+}
