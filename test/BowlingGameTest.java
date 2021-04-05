af5a14a2dcb46ab2b1f1dc04cb80cbc161c58b3c
JeonMinGeon <wjsxorjs@ajou.ac.kr>
2021-04-05 23:54:01 +0900

    The Third Test - 1: @Ignore while Redesigning Game[roll, score]

--- test/BowlingGameTest.java
+++ test/BowlingGameTest.java
@@ -1,32 +1,49 @@
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class BowlingGameTest {
 	
 	private Game g;
 	private int pins;
+	private int n;
 	@Before
 	public void setUp() {
 		g = new Game();
 	}
+
+	public void rollMany(int n,int pins) {
+		for (int i=0;i<n;i++) {
+			g.roll(pins);
+		}
+	}
 	
 	@Test
 	public void testGutterGame() {
-		pins=0;
-		for (int i=0;i<20;i++) {
-			g.roll(pins);
-		}
+//		pins=0;
+//		n=20;
+		rollMany(20,0);
 		assertEquals(0,g.score());
 	}
+
 	@Test
 	public void testAllOnes() {
-		pins=1;
-		for (int i=0;i<20;i++) {
-			g.roll(pins);
-		}
+//		pins=1;
+//		n=20;
+		rollMany(20,1);
 		assertEquals(20,g.score());
+	}
+
+	@Ignore("Redesigning Game")
+	@Test
+	public void testOneSpare() {
+		g.roll(5);
+		g.roll(5); // spare
+		g.roll(3);
+		rollMany(17,0);
+		assertEquals(16,g.score());
 	}
 
 }
