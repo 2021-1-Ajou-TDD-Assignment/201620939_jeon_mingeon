55a5536e19e4be165cd484097e9a9cb109b6b738
JeonMinGeon <wjsxorjs@ajou.ac.kr>
2021-04-05 22:57:06 +0900

    the second test

--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,14 @@
 
 public class Game {
+	private int score  = 0;
 
 	public void roll(int pins) {
-		
-		
+		score += pins;
 	}
 
-	public Object score() {
+	public int score() {
 		// TODO Auto-generated method stub
-		return 0;
+		return score;
 	}
 
 }
